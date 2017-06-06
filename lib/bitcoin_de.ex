a224f5b35eeabd2ae1ad62584c6e619764addb3f
Jonas Zipprick <jonas.zipprick@gmail.com>
2017-06-06 22:29:06 +0200

    intial commit

---
+++ lib/bitcoin_de.ex
@@ -0,0 +1,20 @@
+defmodule BitcoinDe do
+  use GenServer 
+
+  @moduledoc """
+  A Client for bitcoin.de
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BitcoinDe.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bitcoin_de_test.exs
@@ -0,0 +1,8 @@
+defmodule BitcoinDeTest do
+  use ExUnit.Case
+  doctest BitcoinDe
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
